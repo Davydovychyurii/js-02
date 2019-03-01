99d23543bcaca064a89a95966a382b71a617fe6d
Yurii <davydovychyurii@gmail.com>
2019-03-01 18:39:12 +0000

    Inital

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// // // let i = 0;
+// // // while(i <= 100){
+// // //     console.log(i);
+// // //     i++;
+// // // }
+// // let i = 0;
+// // do {
+// //     console.log(i)
+// //     i++
+// // } while (i <= 100);
+// let num = parseFloat (prompt('Enter a number'));
+// while(Number.isNaN(num)){
+//     alert('Please enter a number');
+//     num = parseFloat (prompt('Enter a numbet'));
+// }
+let productName = [];
+productName.push('Tesla');
+productName.push('Corvet');
+productName.push('Renault');
+console.log(productName[2]);
